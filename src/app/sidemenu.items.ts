17ce7593457829fb07f00e5e45cd53b0fb01ea71
lexvenim <lexvenim@gmail.com>
2018-04-03 15:33:46 +0300

    Home page changes

--- src/app/sidemenu.items.ts
+++ src/app/sidemenu.items.ts
@@ -1,34 +1,29 @@
 export const menuItems : any = [
     {
+      auth: true,
       pages: [
-        {page: "profile", icon: "../assets/icon/swords.png", name: "Profile"}
-      ]
-    },
-    {
-      header: "Social",
-      pages: [
-        {page: "chats", icon: "../assets/icon/swords.png", name: "Chats"},
-        {page: "guild", icon: "../assets/icon/swords.png", name: "Guild"}
-      ]
-    },
-    {
-      header: "General",
-      pages: [
-        {page: "adventures", icon: "../assets/icon/ship.png", name: "Adventures"},
-        {page: "battle", icon: "../assets/icon/swords.png", name: "Battle"},
-        {page: "economy", icon: "../assets/icon/mill.png", name: "Economy", auth: true},
-        {page: "sectors", icon: "../assets/icon/castle.png", name: "Island", auth: true},
-        {page: "quests", icon: "../assets/icon/scroll.png", name: "Quests"},
-        {page: "specialists", icon: "../assets/icon/scroll.png", name: "Specialists", auth: true},
-        {page: "timers", icon: "../assets/icon/timer.png", name: "Timers", auth: true},
-        {page: "trade", icon: "../assets/icon/coins.png", name: "Trade"}
+        {page: "profile", icon: "../assets/imgs/menu-profile.png", name: "Profile"},
+        {page: "chats", icon: "../assets/imgs/menu-chats.png", name: "Chats"},
+        {page: "guild", icon: "../assets/imgs/menu-guild.png", name: "Guild"}
       ]
     },
     {
       pages: [
-        {page: "settings", icon: "../assets/icon/swords.png", name: "Settings"},
-        {page: "signin", icon: "../assets/icon/group.png", name: "Sign In", auth: false},
-        {page: "signout", icon: "../assets/icon/shield-1.png", name: "Sign Out", auth: true, logout: true}
+        {page: "adventures", icon: "../assets/imgs/menu-adventures.png", name: "Adventures"},
+        {page: "battle", icon: "../assets/imgs/menu-battle.png", name: "Battle"},
+        {page: "economy", icon: "../assets/imgs/menu-economy.png", name: "Economy", auth: true},
+        {page: "sectors", icon: "../assets/imgs/menu-island.png", name: "Island", auth: true},
+        {page: "timers", icon: "../assets/imgs/menu-notifications.png", name: "Notifications", auth: true},
+        {page: "quests", icon: "../assets/imgs/menu-quests.png", name: "Quests"},
+        {page: "specialists", icon: "../assets/imgs/menu-specialists.png", name: "Specialists", auth: true},
+        {page: "trade", icon: "../assets/imgs/menu-trade.png", name: "Trade"}
+      ]
+    },
+    {
+      pages: [
+        {page: "settings", icon: "../assets/imgs/menu-settings.png", name: "Settings"},
+        {page: "signin", icon: "../assets/imgs/menu-auth.png", name: "Sign In", auth: false},
+        {page: "signout", icon: "../assets/imgs/menu-auth.png", name: "Sign Out", auth: true, logout: true}
       ]
     }
 ];
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  platforms : any = [
+    {name: "desktop", color: "dark", text: "Website"},
+    {name: "logo-chrome", color: "danger", text: "Chrome Extension"},
+    {name: "logo-android", color: "secondary", text: "Android"}
+  ]
+
+  features : any = [
+    {name: "Adventure guides", guest: true},
+    {name: "Battle and Colony Simulators", guest: true},
+    {name: "Questbook", guest: true},
+    {name: "Trade Prices", guest: true},
+    {name: "Economy Planner"},
+    {name: "Guilds and Messaging"},
+    {name: "Island Planning"},
+    {name: "Notifications"},
+    {name: "Specialists and Skils"}
+  ]
+
   constructor(public navCtrl: NavController) {
 
   }
